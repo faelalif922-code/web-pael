b04f97ec558ddbfc9ed71d1181219c66b8e2cf4a
faelalif922-code <faelalif922@gmail.com>
2026-05-05 20:16:21 +0700

    ah

--- script.js
+++ script.js
@@ -45,13 +45,15 @@ const portfolioItems = [
         image: "images/bloger.jpg"   // ganti dengan foto kamu nanti
         link: "https://rafaelaliframadhanxtjkt2.blogspot.com"
     },
+
     {
         id: 2,
         title: "NAT Static + Port Forwarding",
-        description: "Konfigurasi NAT Static dan Port Forwarding pada Mikrotik RouterOS.",
-        image: "images/nat-static.jpg",
+        description: "Konfigurasi NAT Static dan Port Forwarding pada Mikrotik RouterOS agar server lokal bisa diakses dari internet.",
+        image: "images/nat-static.jpg"   // ganti dengan foto kamu nanti
         link: "#"
     },
+
     {
         id: 3,
         title: "Website Portofolio Pribadi",
@@ -59,6 +61,7 @@ const portfolioItems = [
         image: "images/website-portofolio.jpg"
         link: "#"
     },
+
     {
         id: 4,
         title: "Topologi Jaringan Sekolah",
@@ -66,6 +69,7 @@ const portfolioItems = [
         image: "images/topologi-sekolah.jpg"
         link: "#"
     },
+
     {
         id: 5,
         title: "Konfigurasi Mikrotik Hotspot",
